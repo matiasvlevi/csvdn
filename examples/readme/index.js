9323ec1bf02bd4e2796a1a5a8773dfcd2bfdb257
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-12-21 20:53:22 -0500

    init

--- examples/readme/index.js
+++ examples/readme/index.js
@@ -2,30 +2,26 @@ const { CSVDN } = require('../../bin/main.js');
 const fs = require('fs');
 
 let csv = new CSVDN('./sample.csv');
+
 csv.log();
 
-// Normalization Config
 csv.add('Age', {
   mode: 'map'
 });
-csv.add('Level', {
-  mode: 'map',
-  labels: ['junior', 'advanced', 'senior', 'master']
-});
+
 csv.add('Sex', {
   mode: 'bool'
-});
-csv.add('Height', {
+})
+
+csv.add('Heartrate', {
   mode: 'map'
-});
-csv.add('Degree', {
-  mode: 'bool'
-});
+})
+
 
 csv.normalize();
+
 csv.log({ normalized: true });
-csv.log({ align: 'left' });
-csv.log({ header: false });
-csv.log({ join: ' ; ' });
+
 let file = csv.getFile();
+console.log(file)
 fs.writeFileSync('./sample_norm.csv', file, 'utf8');
