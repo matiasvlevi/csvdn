57d1b938fdc716fa3d07710518cc36d9a65f4f33
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-12-21 20:20:56 -0500

    init

---
+++ example/readme/index.js
@@ -0,0 +1,23 @@
+const { CSVDN } = require('../../bin/main.js');
+const fs = require('fs');
+
+let csv = new CSVDN('./sample.csv');
+
+csv.add('Age', {
+  mode: 'map'
+});
+
+csv.add('Sex', {
+  mode: 'bool'
+})
+
+csv.add('Heartrate', {
+  mode: 'map'
+})
+
+csv.normalize();
+
+csv.log({ normalized: true });
+
+let file = csv.getFile();
+fs.writeFileSync('./sample_norm.csv', file, 'utf8');
